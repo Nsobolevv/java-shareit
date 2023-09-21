a0ce2821d45427368c832bba721a7be34baada7d
Cole <lebovski13@yandex.ru>
2023-09-21 16:18:30 +0300

    Sprint 16 rev.2

--- gateway/src/main/java/ru/practicum/request/dto/ItemRequest.java
+++ gateway/src/main/java/ru/practicum/request/dto/ItemRequest.java
@@ -2,7 +2,6 @@ package ru.practicum.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import ru.practicum.item.dto.Item;
 import ru.practicum.user.dto.User;
@@ -14,7 +13,6 @@ import java.util.Set;
 
 
 @Data
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class ItemRequest {
     private Long id;
